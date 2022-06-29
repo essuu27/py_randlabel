eaf37496277d3b37eaa2c5dfcfbea6ff64f8bbce
S Watkins <thinkunix@yahoo.co.uk>
2022-06-29 12:41:36 +0100

    Added comments

--- testcase.py
+++ testcase.py
@@ -1,3 +1,5 @@
+# Simple test case program to show the usage of the randlabel function.
+# It will output ten randomised label strings, each 16 charactyers long.
 import randlabel
 
 for _ in range(1, 10):
